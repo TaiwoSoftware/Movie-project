a2332500da539435bf263c21281642671247cf04
Taiwo AKerele <davcodes24@gmail.com>
2023-09-13 22:07:42 +0100

    checking my data

--- src/components/Header/Main/MainPart.jsx
+++ src/components/Header/Main/MainPart.jsx
@@ -1,9 +1,23 @@
 const MainPart = () => {
-    return (
-        <div className="main-part">
-            <h1>Main part</h1>
-        </div>
-    );
-}
+  const apiKey =
+    "https://api.themoviedb.org/3/movie/157336?api_key=78bcd9519ba51e4395c843281cef3d37";
+  const result = async () => {
+    try {
+      const res = await fetch(apiKey);
+      const data = await res.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error, "Error");
+    }
+  };
+
+  result();
+  return (
+    <div className="main-part">
+      <h1>Main part</h1>
+    </div>
+  );
+};
 
 export default MainPart;
