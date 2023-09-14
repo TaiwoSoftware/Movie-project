dafc1bd462372002fb563fdd7e5fea327267a149
Taiwo AKerele <davcodes24@gmail.com>
2023-09-14 14:50:35 +0100

    checking how i can destructure this data

--- src/components/Header/Main/MainPart.jsx
+++ src/components/Header/Main/MainPart.jsx
@@ -1,15 +1,19 @@
-
 const MainPart = () => {
   const apiKey =
-    'https://api.themoviedb.org/3/discover/movie?api_key=78bcd9519ba51e4395c843281cef3d37';
+    "https://api.themoviedb.org/3/discover/movie?api_key=78bcd9519ba51e4395c843281cef3d37";
   const result = async () => {
     try {
       const res = await fetch(apiKey);
       const data = await res.json();
-
+      
       console.log(data);
-      const {page, results } = data;
-      console.log(results);
+
+      const { page, results } = data;
+      const [ result1, result2, result3  ] = results;
+
+      console.log(result1);
+      console.log(result2);
+      console.log(result3);
     } catch (error) {
       console.log(error, "Error");
     }
