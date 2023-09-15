dca90b918d6754535e197c586b518b1b42d47b4d
Taiwo AKerele <davcodes24@gmail.com>
2023-09-15 18:42:48 +0100

    added images

--- src/components/Grid/Row3.jsx
+++ src/components/Grid/Row3.jsx
@@ -1,7 +1,7 @@
 import Col from "./Col";
-import image1 from "../images/move1.jpeg";
-import image2 from "../images/movie2.jpeg";
-import image3 from "../images/movie3.jpeg";
+import image1 from "https://image.tmdb.org/t/p/w500/6oH378KUfCEitzJkm07r97L0RsZ.jpg";
+import image2 from "https://image.tmdb.org/t/p/w500//n1hqbSCtyBAxaXEl1Dj3ipXJAJG.jpg";
+import image3 from "https://image.tmdb.org/t/p/w500/FQHtuf2zc8suMFE28RyvFt3FJN.jpg";
 import ImbdLogo from "../images/IMDB - Copy.svg";
 import Rotten from "../images/Rotten Tomatoes - Copy.svg";
 const Row3 = () => {
